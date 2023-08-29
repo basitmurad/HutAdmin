65e42e2f9d80cdea69275368884beecc99e58831
Basit Murad <basitmurad7@gmail.com>
2023-08-29 22:02:33 +0500

    done

--- app/src/main/java/com/example/hutsadmin/ui/DashboardActivity.java
+++ app/src/main/java/com/example/hutsadmin/ui/DashboardActivity.java
@@ -1,33 +1,22 @@
 package com.example.hutsadmin.ui;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-import com.example.hutsadmin.R;
 import com.example.hutsadmin.SessionManager;
 import com.example.hutsadmin.databinding.ActivityDashboardBinding;
-import com.example.hutsadmin.fragments.ActiveOrdersFragment;
-import com.example.hutsadmin.fragments.CancelOrdersFragment;
-import com.example.hutsadmin.fragments.DeliverFragment;
 import com.example.hutsadmin.fragments.adapters.MyFragmentAdapter;
 
 import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 public class DashboardActivity extends AppCompatActivity {
 
     private ActivityDashboardBinding binding;
 
-    private ViewPager2 viewPager;
-    private TabLayout tabLayout;
-    private MyFragmentAdapter fragmentAdapter;
 
+    private MyFragmentAdapter myFragmentAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,66 +25,50 @@ public class DashboardActivity extends AppCompatActivity {
 
 
 
-        viewPager = findViewById(R.id.viewPager);
-        tabLayout = findViewById(R.id.tablayout);
 
-        fragmentAdapter = new MyFragmentAdapter(this);
-        viewPager.setAdapter(fragmentAdapter);
-
-        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager,
-                (tab, position) -> {
-                    switch (position) {
-                        case 0:
-                            tab.setText("Active Orders");
-                            break;
-                        case 1:
-                            tab.setText("Cancel Orders");
-                            break;
-                        case 2:
-                            tab.setText("Deliver Orders");
-                            break;
-                    }
-                }
-        );
-        tabLayoutMediator.attach();
-
-
-
-
-
-
-
-
-
-
-    }
-
-    private static class MyFragmentAdapter extends FragmentStateAdapter {
-
-        MyFragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
-            super(fragmentActivity);
-        }
-
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-            switch (position) {
-                case 0:
-                    return new ActiveOrdersFragment();
-                case 1:
-                    return new CancelOrdersFragment();
-                case 2:
-                    return new DeliverFragment();
-                default:
-                    throw new IllegalArgumentException("Invalid tab position: " + position);
+        myFragmentAdapter = new MyFragmentAdapter(DashboardActivity.this);
+        binding.viewPager.setAdapter(myFragmentAdapter);
+        binding.tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                binding.viewPager.setCurrentItem(tab.getPosition());
             }
-        }
 
-        @Override
-        public int getItemCount() {
-            return 3;
-        }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                binding.tablayout.selectTab(binding.tablayout.getTabAt(position));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 
 
     @Override
